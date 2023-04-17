82227720e32b23795e72290c91d48c14700632a1
NODIRA <mir-n-mir@mail.ru>
2023-04-17 15:49:37 +0300

    Xunit test checks sending email and Integration test checks creating new order

---
+++ backend/OnlineStore.Domain.Test/OrderServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.RepositoryInterfaces;
+using OnlineStore.Domain.Services;
+
+namespace OnlineStore.Domain.Test;
+
+public class OrderServiceTests
+{
+    [Fact]
+    public async Task Sending_Email_With_Correct_Parameters_happens()
+    {
+        // Arrange
+        var emailSenderMock = new Mock<IEmailSender>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var accountId = Guid.NewGuid();
+        var productA = Guid.NewGuid();
+        var productB = Guid.NewGuid();
+        var productC = Guid.NewGuid();
+        var order = new Order(Guid.NewGuid(), accountId, new List<OrderItem>()
+        {
+            new (Guid.NewGuid(), productA, 2, 10.99m),
+            new (Guid.NewGuid(), productB, 3, 5.99m),
+            new (Guid.NewGuid(), productC, 1, 19.99m)
+        });
+
+        var city = "TestCity";
+        var address = "TestAddress";
+        var sut = new OrderService(unitOfWorkMock.Object, emailSenderMock.Object);
+
+        // Act
+        await sut.NotifyAboutPlacedOrder(order, city, address);
+
+        // Assert
+        emailSenderMock.Verify(es => es.Send(ShopConfig.ManagerEmail, "Новый заказ отправлен", It.IsAny<string>(), default), Times.Once);
+    }
+}
--- backend/OnlineStore.WebApi.IntegrationTests/OrderEndpointsTests.cs
+++ backend/OnlineStore.WebApi.IntegrationTests/OrderEndpointsTests.cs
@@ -1,71 +1,37 @@
-﻿using Moq;
-using Bogus;
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
 using OnlineStore.Domain.Entities;
-using OnlineStore.Domain.RepositoryInterfaces;
-using OnlineStore.Domain.Services;
 
 namespace OnlineStore.WebApi.IntegrationTests;
 
-public class OrderEndpointsTests: IClassFixture<CustomWebApplicationFactory<Program>>
+public class OrderEndpointsTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
-    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
-    private readonly Mock<IEmailSender> _emailSenderMock;
-    private readonly OrderService _orderService;
-    private readonly Faker _faker;
+    private readonly HttpClient _httpClient;
 
-    public OrderEndpointsTests()
+    public OrderEndpointsTests(CustomWebApplicationFactory<Program> factory)
     {
-        _unitOfWorkMock = new Mock<IUnitOfWork>();
-        _emailSenderMock = new Mock<IEmailSender>();
-        _orderService = new OrderService(_unitOfWorkMock.Object, _emailSenderMock.Object);
-        _faker = new Faker();
+        _httpClient = factory.CreateClient();
     }
 
     [Fact]
-    public async Task PlaceOrderAndCreateNew_ShouldCreateOrderAndNotifyManager()
+    public async Task Creating_New_Order_happens()
     {
         // Arrange
         var accountId = Guid.NewGuid();
-        var city = _faker.Address.City();
-        var address = _faker.Address.FullAddress();
-        var productA = Guid.NewGuid();
-        var productB = Guid.NewGuid();
-        var productC = Guid.NewGuid();
-
-        var cart = new Cart(Guid.Empty, accountId, new List<CartItem>()
-        {
-            new (Guid.NewGuid(), productA, 2, 10.99m),
-            new (Guid.NewGuid(), productB, 3, 5.99m),
-            new (Guid.NewGuid(), productC, 1, 19.99m)
-        });
-
-        var order = new Order(Guid.NewGuid(), accountId, new List<OrderItem>()
-        {
-            new (Guid.NewGuid(), productA, 2, 10.99m),
-            new (Guid.NewGuid(), productB, 3, 5.99m),
-            new (Guid.NewGuid(), productC, 1, 19.99m)
-        });
-
-        _unitOfWorkMock.Setup(uow => uow.CartRepository.GetByAccountId(accountId, default))
-            .ReturnsAsync(cart);
-
-        _unitOfWorkMock.Setup(uow => uow.OrderRepository.Add(order, default))
-            .Returns(Task.CompletedTask);
-
-        _emailSenderMock.Setup(es => es.Send(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), default))
-            .Returns(Task.CompletedTask);
+        var city = "TestCity";
+        var address = "TestAddress";
 
         // Act
-        var result = await _orderService.PlaceOrderAndCreateNew(accountId, city, address, CancellationToken.None);
+        var response = await _httpClient.PostAsJsonAsync($"/api/orders/place-order/{accountId}", new { city, address });
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var order = await response.Content.ReadFromJsonAsync<Order>();
 
         // Assert
-        _unitOfWorkMock.Verify(uow => uow.CartRepository.GetByAccountId(accountId, default), Times.Once);
-        // _unitOfWorkMock.Verify(uow => uow.OrderRepository.Add(order, default), Times.Once);
-        _emailSenderMock.Verify(es => es.Send(ShopConfig.ManagerEmail, "Новый заказ отправлен", It.IsAny<string>(), default), Times.Once);
-
-        // Assert.Equal(order.Id, result.Id);
-        Assert.Equal(accountId, result.AccountId);
-        Assert.Equal(order.Items.Count, result.Items.Count);
-        Assert.Equal(order.GetTotalPrice(), result.GetTotalPrice());
+        order.Should().NotBeNull();
+        order!.AccountId.Should().Be(accountId);
+        order.Items.Should().NotBeNullOrEmpty();
+        order.Items.Should().HaveCount(2);
     }
 }
