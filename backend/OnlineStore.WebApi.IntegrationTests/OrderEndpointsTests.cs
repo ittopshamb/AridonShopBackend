8c5fc9b5c05cae0e1e31615b06b7675d7da8c7c3
NODIRA <mir-n-mir@mail.ru>
2023-04-19 12:16:48 +0300

    Creating_New_Order_with_Invalid_Product_Id_Fails

--- backend/OnlineStore.WebApi.IntegrationTests/OrderEndpointsTests.cs
+++ backend/OnlineStore.WebApi.IntegrationTests/OrderEndpointsTests.cs
@@ -57,4 +57,41 @@ public class OrderEndpointsTests : IClassFixture<CustomWebApplicationFactory<Pro
         order.Items.Should().NotBeNullOrEmpty();
         order.Items.Should().HaveCount(1);
     }
+    
+    [Fact]
+    public async Task Creating_New_Order_with_Invalid_Product_Id_Fails()
+    {
+        // Arrange
+        var registerRequest = new RegisterRequest()
+        {
+            Email = _faker.Person.Email,
+            Name = _faker.Person.UserName,
+            Password = _faker.Internet.Password()
+        };
+        var httpClient = _factory.CreateClient();
+        var client = new ShopClient(httpClient: httpClient);
+        var registerResponse = await client.Register(registerRequest);
+        var accountId = registerResponse.AccountId;
+        var address = _faker.Person.Address;
+        var items = new List<OrderItemRequest>()
+        {
+            new()
+            {
+                ProductId = Guid.Empty,
+                Quantity = 1,
+                Price    = 50
+            }
+        };
+
+        var orderRequest = new PlaceOrderRequest()
+        {
+            AccountId = accountId,
+            Address = address.Suite + ", " + address.Street,
+            City = address.City,
+            Items = items
+        };
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ArgumentException>(() => client.PlaceOrder(orderRequest));
+    }
 }
